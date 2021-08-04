c312a4789d7807809b564f5dcab32cb4ccecc800
Alexandre Pedro <alexandrecpedro@gmail.com>
2021-08-04 20:19:17 -0300

    DOM Structure

--- script.js
+++ script.js
@@ -2,28 +2,35 @@
 // Exercício 1
 console.log("Arquivo vinculado !");
 // Exercício 2
-document.querySelector('div.avatar-content').innerHTML = '<img class="avatar" src="/img/avatar-example.png"> <span>Nome Usuário</span>';
+let nome = document.querySelector('div.avatar-content').innerHTML = '<img class="avatar" src="/img/avatar-example.png"> <span>Nome Usuário</span>';
 // Exercício 3
-document.querySelector('input[name="name"]').placeholder = 'Primeiro Nome';
+let primeiroNome = document.querySelector('input[name="name"]').placeholder = 'Primeiro Nome';
 // Exercício 4
 document.getElementsByClassName('avatar-content').innerHTML = '<img class="avatar" src="/img/avatar-example.png"> <span>Ale</span>';
 
 // ATIVIDADE 2
 // Exercício 1
-var example = document.querySelectorAll("input")
-for (i=0;i<example.length;i++){
-    example[i].style.borderRadius="4px"
-};
+// var example = document.querySelectorAll("input")
+// for (i=0;i<example.length;i++){
+//     example[i].style.borderRadius="4px"
+// };
+// Outra forma
+let bordas = document.querySelectorAll('input');
+for (let borda of bordas) {
+    borda.style.borderRadius = "4px"
+    borda.style.width = "100%"
+}
+
 // Exercício 2
-var example = document.querySelectorAll("input")
-for (i=0;i<example.length;i++){
-    example[i].style.display="flex";
-    example[i].style.flexWrap="wrap";
-    example[i].style.flexDirection="column";
-    example[i].style.justifyContent="center";
-    example[i].style.alignItems="center";
-    example[i].style.width="100%";
-};
+// var example = document.querySelectorAll("input")
+// for (i=0;i<example.length;i++){
+//     example[i].style.display="flex";
+//     example[i].style.flexWrap="wrap";
+//     example[i].style.flexDirection="column";
+//     example[i].style.justifyContent="center";
+//     example[i].style.alignItems="center";
+//     example[i].style.width="100%";
+// };
 // Exercício 3
 document.body.style.backgroundColor="#fafafa";
 // Exercício 4
